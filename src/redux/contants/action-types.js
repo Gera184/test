135cca76ee8876e1c41eef64e13a213a3579ce4e
Gera184 <oogeraoo159@gmail.com>
2021-11-23 16:50:00 +0200

    test

--- src/redux/contants/action-types.js
+++
@@ -1,3 +0,0 @@
-export const ActionTypes = {
-  SET_USER_DETAILS: "SET_USER_DETAILS",
-};
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { userDeatilsReducer } from "./userDeatilsReducer";
-
-const reducers = combineReducers({
-  user: userDeatilsReducer,
-});
-
-export default reducers;
